f28b1b8cfdbc6ca7565d96172c85591ba0a6b131
postig0x <postigo.kura@gmail.com>
2024-08-02 05:21:59 +0000

    could not sudo to make new file, changed output file location

--- analyze.sh
+++ analyze.sh
@@ -4,13 +4,13 @@
 # read through each line of the log file
 # search for keyword that indicates suspicious login attempts
 # if the line contains the keyword, write the line to a new file:
-#       /var/log/suspicious_activity.log
+#       suspicious_activity.log
 # run the script daily crontab -e
 
 # regex list of words that will help flag suspicious activity
 sus_keys="failed|error|unauthorized" # | === "or"
 
-# search for lines containing these words (case-insensitive), and output to desired file (/var/log/suspicious_activity.log)
+# search for lines containing these words (case-insensitive), and output to desired file (suspicious_activity.log)
 grep -i -E "$sus_keys" /var/log/auth_log.log >> suspicious_activity.log
 #    -i               -> makes the search case-insensitive
 #       -E            -> allows for the use of | (or) in the search arg
